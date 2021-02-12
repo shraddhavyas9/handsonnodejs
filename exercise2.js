dce958db3737dce005dc28771b2761010eebd29e
shraddhavyas9 <78908859+shraddhavyas9@users.noreply.github.com>
2021-02-12 17:49:43 +0530

    Update exercise2.js

--- exercise2.js
+++ exercise2.js
@@ -1,8 +1,8 @@
 var buffer1 = new Buffer(100);
 for(var i = 0; i < buffer1.length; i ++) {
- buffer[i] = i;
+ buffer1[i] = i;
  }
- console.log(buffer);
+ console.log(buffer1);
 
  var buffer2 = buffer.slice(40, 60);
 
